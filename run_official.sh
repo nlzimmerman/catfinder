980ac1ea76effdaf28f9890d291ce9f5dc0276cd
Nicholas Lewkow <nicholas.lewkow@mheducation.com>
2016-08-01 14:21:10 -0400

    print port number for local connection

--- run_official.sh
+++ run_official.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker run -it -p 8889:8888 tensorflow/tensorflow
+echo '----------------------------------------------'
 echo 'Running tensorflow Jupyter server on port 8889'
+echo '----------------------------------------------'
+echo ''
+
+docker run -it -p 8889:8888 tensorflow/tensorflow
