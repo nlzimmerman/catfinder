547fc6f0e9b3cb823e08ade624189e3521afffb4
Neil L Zimmerman <neil.l.zimmerman@gmail.com>
2016-08-08 13:51:11 -0400

    fixed a typo

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 8888:8888 -m 16g --cpu-shares=4 -v $(pwd)/jup:/home/jovyan/work/jup -v $(pwd)/data:/home/jovyan/work/data -v $(pwd)/python:/home/joyvan/work/python jupyter/all-spark-notebook
+docker run -d -p 8888:8888 -m 16g --cpu-shares=4 -v $(pwd)/jup:/home/jovyan/work/jup -v $(pwd)/data:/home/jovyan/work/data -v $(pwd)/python:/home/jovyan/work/python jupyter/all-spark-notebook
